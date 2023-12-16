5efdc8ca9cb10f6b30aa1cc4299ffa08318da7f3
MSupreeth123 <89769100+MSupreeth123@users.noreply.github.com>
2023-12-16 11:27:21 +0530

    Add files via upload

---
+++ electricitybill/src/electricitybill/billcalcu.java
@@ -0,0 +1,21 @@
+package electricitybill;
+
+public class billcalcu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int units = 280;
+		double billTopay = 0;
+		if (units < 100)
+		{
+			billTopay = units * 1.20;
+		}
+		else if(units < 300)
+		{
+			billTopay = 100 * 1.20 + (units -100) *2;
+		}
+		
+		System.out.println ("The electricity bill for " +units+ " is : " + billTopay);
+	}
+
+	}
